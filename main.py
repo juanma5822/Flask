0fea6b62d71d789471e415ddfcf324bbfbd9f315
Juan Romero <juan.romero5822@gmail.com>
2022-08-12 18:10:20 -0500

    Creando depedencias

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from flask import Flask, request, make_response, redirect, render_template, session
-from flask_bootstrap import Bootstrap
+from flask import Flask, request, make_response, redirect, render_template, session,url_for,flash
+import unittest
+from app import create_app
+
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
-
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+    
+app = create_app()
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()],)
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -42,25 +45,33 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
-
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form
+        'username' : username
+
     }
 
     return render_template('hello.html', **context)
 
-@app.route('/hello')
-def hello():
+@app.route('/sign_ig',methods=['GET','POST'])
+def sign_in():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
-
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form
+        'login_form': login_form,
+        'username' : username
     }
+    if login_form.validate_on_submit():
+        username=login_form.username.data
+        session['username']=username
 
-    return render_template('hello.html', **context)
+        flash('Ingreso correcto')
+
+        return redirect(url_for('index'))
+        
+    return render_template('sign_in.html', **context)
