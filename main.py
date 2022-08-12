f05c769b03f4be3440c591dd7877496d630af5c3
Juan Romero <juan.romero5822@gmail.com>
2022-08-12 11:05:35 -0500

    aplicando estilos boostrap

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
-@app.errorhandler(500)
-def internal_server(error):
-    return render_template('500.html',error=error)
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()],)
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
@@ -21,17 +34,33 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
+    }
+
+    return render_template('hello.html', **context)
+
+@app.route('/hello')
+def hello():
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
