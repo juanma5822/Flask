f0a9960b5cb6a970a881eb3f873780991c23c585
Juan Romero <juan.romero5822@gmail.com>
2022-08-17 18:58:08 -0500

    Modificados los templates

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from flask import Flask, request, make_response, redirect, render_template, session,url_for,flash
+from flask import request, make_response, redirect, render_template, session,url_for,flash
 import unittest
+
 from app import create_app
-
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from app.forms import LoginForm
+from app.auth import auth
 
 
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-    
+
+
 app = create_app()
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
@@ -55,23 +51,4 @@ def hello():
 
     return render_template('hello.html', **context)
 
-@app.route('/sign_ig',methods=['GET','POST'])
-def sign_in():
-    user_ip = session.get('user_ip')
-    login_form = LoginForm()
-    username = session.get('username')
-    context = {
-        'user_ip': user_ip,
-        'todos': todos,
-        'login_form': login_form,
-        'username' : username
-    }
-    if login_form.validate_on_submit():
-        username=login_form.username.data
-        session['username']=username
 
-        flash('Ingreso correcto')
-
-        return redirect(url_for('index'))
-        
-    return render_template('sign_in.html', **context)
